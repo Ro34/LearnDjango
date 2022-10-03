edd74e4dbe3d2e7a05e9582407656ab99acd06c1
Ro34 <pengjingwei@yijiahe.com>
2022-10-03 15:57:10 +0800

    Hello_world2

--- HelloWorld/views.py
+++ HelloWorld/views.py
@@ -3,5 +3,5 @@ from django.shortcuts import render
 
 def run(request):
     view_list = ['a', 'b', 'c']
-    context = {'hello': ['a', 'b', 'c']}
+    context = {'hello': view_list}
     return render(request, 'runoob.html', context)
